e7983b6bab4a782c62b4acfea9ef6c928ccb23e0
haini000 <haini000@naver.com>
2025-12-18 14:46:57 +0900

    feat: modules add

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import {renderHeader} from './modules/header.js';
+import {renderFooter} from './modules/footer.js';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderHeader();
+    renderFooter();
+});
+
---
+++ js/modules/header.js
@@ -0,0 +1,25 @@
+export function renderHeader() {
+  const header = document.querySelector('#header');
+
+  header.innerHTML =
+    `
+      <div class="container">
+        <div class="topLeft">
+          <div class="toggleMenu">
+            <span class="material-symbols-outlined">menu</span>
+          </div>
+          <h1><a href="index.html"><img src="/images/logo.svg" alt="logo"><span class="hidden">ROUNZ</span></a></h1>
+        </div>
+        <ul class="topRight">
+          <li><a href=""><span class="material-symbols-outlined">account_circle</span></a></li>
+          <li><a href=""><span class="material-symbols-outlined">shopping_cart</span></a></li>
+          <li>
+            <ul class="account">
+              <li><a href="">로그인</a></li>
+              <li><a href="">회원가입</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    `
+}
